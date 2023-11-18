489c58dda4471a9dd18965e7653ff64e61d69938
Roberto <rob.braga00@gmail.com>
2023-11-18 01:41:29 +0000

    added context need more funcs

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,6 +6,7 @@ import {
   AccountInputTitle,
 } from "./AccountStyles";
 import { useState } from "react";
+import axios from "axios";
 
 interface Account {
   email: string;
@@ -20,9 +21,21 @@ const LogIn = () => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAccountInfo((prev) => ({ ...prev, [e.target.type]: e.target.value }));
   };
-  const submit = (e: React.FormEvent) => {
+  const submit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("test");
+    try {
+        const response = await axios.post(
+          "https://x8ki-letl-twmt.n7.xano.io/api:mxGtNEgl/auth/login",
+          { email: accountInfo.email, password: accountInfo.password }
+        );
+  
+        localStorage.setItem("authToken", response.data.authToken);
+  
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    
   };
   return (
     <AccountContainer>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
+import UserProvider from "@/Components/AppContext";
 import Layout from "@/Components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
   );
 }
