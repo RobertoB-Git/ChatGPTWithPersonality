4f8f8cc3962c7bbc676785343076331bebf8dfdd
Roberto <rob.braga00@gmail.com>
2023-11-25 14:37:04 +0000

    most done need to fix types with styled components

--- src/Components/chatPage/ChatPageStyles.tsx
+++ src/Components/chatPage/ChatPageStyles.tsx
@@ -1,10 +1,20 @@
-import styled from "@emotion/styled";
+import styled, { StyledComponent } from "@emotion/styled";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 export const ChatContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 3fr;
+  grid-template-columns: minmax(205px,20rem) minmax(30rem,100%);
   /* position: relative; */
-  height: 100vmax;
+  /* height: 100rem; */
+  max-height: 46rem;
+  /* overflow: hidden; */
+  overflow: none;
+
+  @media (max-width: 685px) {
+    display: block;
+    max-height: none;
+
+  }
 `;
 
 export const SideBar = styled.div`
@@ -13,7 +23,44 @@ export const SideBar = styled.div`
   flex-direction: column;
   gap: 2rem;
   align-items: center;
+  max-height: 46rem;
+  transition: 0.5s;
+
+  @media (max-width: 685px) {
+    position: fixed;
+    height: 100%;
+    width: ${(props)=> (props.sideButton ? 0 : '20rem')};
+    z-index: 1;
+    top: 0;
+    left: 0;
+    overflow-x: hidden;
+    padding-top: 60px;
+    text-align: right;
+  }
+
 `;
+
+export const SideButton = styled.button`
+  position: fixed;
+  left: ${(props)=> (props.sideButton ? 0 : '20rem')};
+  width: 1rem;
+  height: 5rem;
+  background-color: black;
+  color: white;
+  margin-top: 1rem;
+  transition: 0.5s;
+
+  @media (min-width: 685px) {
+    display: none;
+  }
+
+`
+// export const SideBarContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 2rem;
+//   align-items: center;
+// `;
 export const SideBarTitle = styled.h3`
   margin-top: 2rem;
   font-size: large;
@@ -26,24 +73,68 @@ export const SideBarItems = styled.div`
   width: 90%;
   justify-content: space-between;
 `;
-export const SideBarItemsimg = styled.img`
+export const SmallImg = styled.img`
+  object-fit: cover;
   aspect-ratio: 1/1;
   width: 4rem;
+  border-radius: 0.5rem;
 `;
 export const ChatBox = styled.div`
   background-color: #e2e2e2;
   display: flex;
   flex-direction: column;
   align-items: center;
+  max-height: 46rem;
 `;
-export const ChatMessages = styled.div`
+export const ChatMessagesContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column-reverse;
+  gap: 2rem;
+  padding: 2rem;
+  /* justify-content: space-between; */
   background-color: #e2e2e2;
   height: 93%;
+
+  overflow: hidden;
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+  /* max-width: */
+  /* height: calc(100vh ); */
+`;
+export const ChatMessages = styled.div`
+  text-align: ${(props) => (props.gpt ? "left" : "right")};
+  background-color: #939393;
+  display: flex;
+  /* justify-content: space-between; */
+  align-items: center;
+  align-items: flex-start;
+  gap: 1rem;
+  border-radius: 1rem;
+
+  /* width: fit-content; */
+`;
+// export const ChatMessagesUser = styled.div`
+//   background-color: #e2e2e2;
+// `;
+
+export const DefaultImg = styled.div`
+  overflow: hidden;
+  /* aspect-ratio: 1/1; */
+  width: 4rem;
+  border-radius: 0.5rem;
+  flex-shrink: 0;
+`;
+
+export const Text = styled.p`
+  padding: 1rem 0 1rem 0;
 `;
 
 export const ChatInput = styled.input`
   background-color: #010139;
   color: white;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
   padding-left: 1rem;
   border-radius: 50rem;
   /* position: absolute; */
@@ -53,4 +144,6 @@ export const ChatInput = styled.input`
   width: 70%;
   height: 2rem;
   bottom: 0;
+
+ 
 `;
--- src/pages/chat/[Name].tsx
+++ src/pages/chat/[Name].tsx
@@ -3,29 +3,64 @@ import {
   ChatContainer,
   ChatInput,
   ChatMessages,
+  ChatMessagesContainer,
   SideBar,
   SideBarItems,
-  SideBarItemsimg,
+  SmallImg,
   SideBarTitle,
+  DefaultImg,
+  Text,
+  SideBarContainer,
+  SideButton,
 } from "@/Components/chatPage/ChatPageStyles";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
+import avatar from '../../../public/avatar.png'
+import Image from "next/image";
 interface Celeb {
-    id: number;
-    Name: string;
-    Image: string;
-  }
-  
+  id: number;
+  Name: string;
+  Image: string;
+}
+
 type List = Celeb[];
 
 const Chat = () => {
   const router = useRouter();
   console.log(router.query.Name);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [celeb, setCeleb] = useState<Celeb>();
   const [celebList, setCelebList] = useState<List>([]);
+  const [chatMessages, setChatMessages] = useState([
+    {
+      role: "gpt",
+      message: "Gpt message1",
+    },
+    {
+      role: "user",
+      message: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum est dignissimos eaque voluptatum placeat temporibus alias eius labore sequi perferendis eos nostrum tempore natus, impedit cupiditate quisquam maxime? Odit nihil culpa minima aliquam iure esse consequatur vitae est eaque. Est iste vel provident unde dignissimos accusantium, fugit repudiandae nesciunt distinctio sapiente perspiciatis quod beatae veritatis magnam officia dolorem possimus suscipit hic aliquam velit aspernatur, minima a mollitia! Quam maiores ducimus nihil nam quisquam quidem officia porro commodi est dicta blanditiis eveniet quia neque doloribus, non atque a quibusdam hic voluptate tempore totam. Nobis, laboriosam voluptatibus reprehenderit perferendis quasi earum sed",
+    },
+    {
+      role: "user",
+      message: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum est dignissimos eaque voluptatum placeat temporibus alias eius labore sequi perferendis eos nostrum tempore natus, impedit cupiditate quisquam maxime? Odit nihil culpa minima aliquam iure esse consequatur vitae est eaque. Est iste vel provident unde dignissimos accusantium, fugit repudiandae nesciunt distinctio sapiente perspiciatis quod beatae veritatis magnam officia dolorem possimus suscipit hic aliquam velit aspernatur, minima a mollitia! Quam maiores ducimus nihil nam quisquam quidem officia porro commodi est dicta blanditiis eveniet quia neque doloribus, non atque a quibusdam hic voluptate tempore totam. Nobis, laboriosam voluptatibus reprehenderit perferendis quasi earum sed",
+    },
+    {
+      role: "user",
+      message: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum est dignissimos eaque voluptatum placeat temporibus alias eius labore sequi perferendis eos nostrum tempore natus, impedit cupiditate quisquam maxime? Odit nihil culpa minima aliquam iure esse consequatur vitae est eaque. Est iste vel provident unde dignissimos accusantium, fugit repudiandae nesciunt distinctio sapiente perspiciatis quod beatae veritatis magnam officia dolorem possimus suscipit hic aliquam velit aspernatur, minima a mollitia! Quam maiores ducimus nihil nam quisquam quidem officia porro commodi est dicta blanditiis eveniet quia neque doloribus, non atque a quibusdam hic voluptate tempore totam. Nobis, laboriosam voluptatibus reprehenderit perferendis quasi earum sed",
+    },
+    {
+      role: "user",
+      message: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum est dignissimos eaque voluptatum placeat temporibus alias eius labore sequi perferendis eos nostrum tempore natus, impedit cupiditate quisquam maxime? Odit nihil culpa minima aliquam iure esse consequatur vitae est eaque. Est iste vel provident unde dignissimos accusantium, fugit repudiandae nesciunt distinctio sapiente perspiciatis quod beatae veritatis magnam officia dolorem possimus suscipit hic aliquam velit aspernatur, minima a mollitia! Quam maiores ducimus nihil nam quisquam quidem officia porro commodi est dicta blanditiis eveniet quia neque doloribus, non atque a quibusdam hic voluptate tempore totam. Nobis, laboriosam voluptatibus reprehenderit perferendis quasi earum sed",
+    },
+
+    {
+      role: "gpt",
+      message: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum est dignissimos eaque voluptatum placeat temporibus alias eius labore sequi perferendis eos nostrum tempore natus, impedit cupiditate quisquam maxime? Odit nihil culpa minima aliquam iure esse consequatur vitae est eaque. Est iste vel provident unde dignissimos accusantium, fugit repudiandae nesciunt distinctio sapiente perspiciatis quod beatae veritatis magnam officia dolorem possimus suscipit hic aliquam velit aspernatur, minima a mollitia! Quam maiores ducimus nihil nam quisquam quidem officia porro commodi est dicta blanditiis eveniet quia neque doloribus, non atque a quibusdam hic voluptate tempore totam. Nobis, laboriosam voluptatibus reprehenderit perferendis quasi earum sed",
+    },
+  ]);
+  const [sideButton, setSideButton] = useState(false);
+
 
   useEffect(() => {
     const getCeleb = async () => {
@@ -43,31 +78,51 @@ const Chat = () => {
         console.log(allCelebList.data);
         setCelebList(allCelebList.data);
       } catch (error) {
-        console.log(error);
+        console.log(error.message);
       }
     };
 
     getCeleb();
   }, []);
 
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSideBarButton = () => {
+    setSideButton(!sideButton)
   }
 
   return (
     <ChatContainer>
-      <SideBar>
+      <SideBar sideButton={sideButton}>
         <SideBarTitle>Celeb List</SideBarTitle>
+    
+        <SideButton sideButton={sideButton} onClick={handleSideBarButton}>{sideButton? ">": "<"}</SideButton>
         {celebList.map((celeb) => {
-          return <SideBarItems>
-            <SideBarItemsimg src={celeb.Image}></SideBarItemsimg>
-            {celeb.Name} 
-            </SideBarItems>;
+          return (
+            <SideBarItems>
+              <SmallImg src={celeb.Image}></SmallImg>
+              {celeb.Name}
+            </SideBarItems>
+          );
         })}
       </SideBar>
       <ChatBox>
-        <ChatMessages></ChatMessages>
-        <ChatInput value={input} onChange={handleChange} ></ChatInput>
+        <ChatMessagesContainer>
+          {chatMessages.map((chat) => {
+            if (chat.role == "gpt") {
+              return (
+                <ChatMessages gpt>
+                  <SmallImg src={celeb?.Image} /> 
+                  <Text>{chat.message}</Text>
+                </ChatMessages>
+              );
+            }
+            return <ChatMessages> <Text>{chat.message}</Text> <DefaultImg><Image objectFit="cover" alt="avatar" src={avatar} /> </DefaultImg></ChatMessages>;
+          })}
+        </ChatMessagesContainer>
+        <ChatInput value={input} onChange={handleChange}></ChatInput>
       </ChatBox>
     </ChatContainer>
   );
