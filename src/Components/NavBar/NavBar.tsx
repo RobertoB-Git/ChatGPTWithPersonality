8fa52ebd111a6ebd4dbc345ea8659432a2b30e90
Roberto <rob.braga00@gmail.com>
2023-12-16 15:29:14 +0000

    made navbar phone friendly

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,31 +1,40 @@
 import Link from "next/link";
 import {
   FlexDiv,
+  FlexDiv2,
   NavBarIcon,
   NavbarContainer,
   NavbarMenu,
+  PhoneMenu,
+  PhoneMenuinner,
+  PhoneMenuinnerContainer,
 } from "./NavBarStyles";
 import SearchBar from "../SearchBar";
 import Icon from "../../../public/celebchat-high-resolution-logo.png";
 import Icon2 from "../../../public/celebchat-high-resolution-logo-transparent.svg";
 import Lupa from "../../../public/zoom-svgrepo-com.svg";
 import Image from "next/image";
-import { useContext } from "react";
+import burger from "../../../public/burger-menu-svgrepo-com.svg";
+import { useContext, useState } from "react";
 import { AppContext } from "../AppContext";
 
 const Navbar = () => {
   const { logOut, user } = useContext(AppContext);
+  const [burgerMenu, setBurgerMenu] = useState(false);
   // console.log(user);
   // logOut() ??
+  const handleBurger = () => {
+    setBurgerMenu(!burgerMenu);
+  };
   return (
     <NavbarContainer>
       {(!user && (
-        <FlexDiv>
+        <FlexDiv2>
           <Link href={"/Login"}>Log in</Link>
           <Link href={"/Register"}>Sign up</Link>
-        </FlexDiv>
+        </FlexDiv2>
       )) || (
-        <FlexDiv>
+        <FlexDiv2>
           {user?.email}
           <NavbarMenu
             onClick={() => {
@@ -33,8 +42,36 @@ const Navbar = () => {
             }}>
             Log out
           </NavbarMenu>
-        </FlexDiv>
+        </FlexDiv2>
       )}
+
+      <PhoneMenu >
+        <Image
+          width={30}
+          height={30}
+          src={burger.src}
+          alt="menu"
+          onClick={handleBurger}
+        />
+        <PhoneMenuinner burgerButton={burgerMenu}>
+          {(!user && (
+            <PhoneMenuinnerContainer>
+              <Link href={"/Login"}>Log in</Link>
+              <Link href={"/Register"}>Sign up</Link>
+            </PhoneMenuinnerContainer>
+          )) || (
+            <PhoneMenuinnerContainer>
+              <p>{user?.email}</p>
+              <NavbarMenu
+                onClick={() => {
+                  logOut();
+                }}>
+                Log out
+              </NavbarMenu>
+            </PhoneMenuinnerContainer>
+          )}
+        </PhoneMenuinner>
+      </PhoneMenu>
 
       <FlexDiv>
         <Image src={Lupa} alt="Search" />
--- src/Components/NavBar/NavBarStyles.tsx
+++ src/Components/NavBar/NavBarStyles.tsx
@@ -1,5 +1,9 @@
 import styled from "@emotion/styled";
 
+interface ButtonProps {
+  burgerButton: boolean;
+}
+
 export const NavbarContainer = styled.nav`
   width: 100%;
   height: 4rem;
@@ -12,6 +16,7 @@ export const NavbarContainer = styled.nav`
   a {
     font-weight: bold;
     text-align: center;
+    display: none;
 
   }
 `;
@@ -23,9 +28,50 @@ export const FlexDiv = styled.div`
   text-align: center;
 
 `;
+export const FlexDiv2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  font-weight: bold;
+  text-align: center;
+
+  @media (max-width: 685px) {
+    display: none;
+  }
+`;
 
 export const NavbarMenu = styled.nav`
+cursor: pointer;
  
+`;
+export const PhoneMenu = styled.div`
+ display: none;
+ @media (max-width: 685px) {
+    display: block;
+  
+  }
+`;
+export const PhoneMenuinner = styled.div<ButtonProps>`
+ width: 20rem;
+ background-color: #c7c7c7;
+ position: fixed;
+ left: 0;
+ top: 4rem;
+ z-index: 20;
+ flex-direction: column;
+ @media (max-width: 685px) {
+    /* height: 0; */
+    overflow: hidden;
+    height: ${(props) => (props.burgerButton ? 0 : "5rem")};
+  }
+`;
+export const PhoneMenuinnerContainer = styled.div`
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ text-align: center;
+ font-weight: bold;
+ gap: 1rem;
 `;
 
 export const NavBarIcon = styled.img`
